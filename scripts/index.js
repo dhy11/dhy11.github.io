3add6d02e10f44927939322dd920924bfac57135
dhy11 <73905280+dhy11@users.noreply.github.com>
2020-11-09 22:14:19 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Nov 09, 2020',
+  birthdate: 'Nov 09, 2021',
   name: 'DHIYA AFIFAH'
 };
 
